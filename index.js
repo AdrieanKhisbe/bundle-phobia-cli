a630810dd58723a42d76abff1519b452275fb5ad
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-09-12 20:05:59 +0200

    refactor introduce a fetchAndPresent function

--- index.js
+++ index.js
@@ -14,21 +14,26 @@ const argv = require('yargs')
 if (!module.parent) {
     const package = argv._[0];
 
-    let computeMessage, fetchPromise, processFunction;
+    const fetchAndPresent = (computeMessage, fetchPromise, processFunction) => {
+        const spinner = ora(computeMessage).start()
+        return fetchPromise
+            .finally(() => spinner.stop().clear())
+            .then(processFunction)
+            .catch(err => console.error(c.red.bold('Error happened:'), err.message));
+    }
 
     if ('range' in argv && 'r' in argv) {
         const nversion = argv.range ? argv.range : (argv.range === undefined ? 8 : 'all');
-        computeMessage = `Fetching stats for ${c.cyan(nversion)} versions of package ${c.dim.underline(package)}`;
-        fetchPromise = fetchPackageStatsByVersion(package, argv.range)
-        processFunction = packageStates => packageStates.map(packageState => console.log(syntheticView(packageState)));
+        fetchAndPresent(
+            `Fetching stats for ${c.cyan(nversion)} last versions of package ${c.dim.underline(package)}`,
+            fetchPackageStatsByVersion(package, argv.range),
+            packageStates => packageStates.map(packageState => console.log(syntheticView(packageState)))
+        );
     } else {
-        computeMessage = `Fetching stats for package ${c.dim.underline(package)}`
-        fetchPromise = fetchPackageStats(package)
-        processFunction = packageState => console.log(syntheticView(packageState));
+        fetchAndPresent(
+            `Fetching stats for package ${c.dim.underline(package)}`,
+            fetchPackageStats(package),
+            packageState => console.log(syntheticView(packageState))
+        );
     }
-    const spinner = ora(computeMessage).start()
-    fetchPromise
-        .finally(() => spinner.stop().clear())
-        .then(processFunction)
-        .catch(err => console.error(c.red.bold('Error happened:'), err.message));
 }
