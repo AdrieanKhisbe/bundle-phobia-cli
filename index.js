d8da08e642f9e28657265bea110a0f84123e81c7
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-09-12 08:52:45 +0200

    init: prettier printing of errors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const c = require('chalk');
 const fetchPackageStats = require('./lib/fetch-package-stats');
 const {syntheticView} = require('./lib/cli-views');
 
 if (!module.parent) {
     fetchPackageStats(process.argv[2])
         .then(packageState => console.log(syntheticView(packageState)))
-        .catch(err => console.error('Error happened:', err.message));
+        .catch(err => console.error(c.red.bold('Error happened:'), err.message));
 }
