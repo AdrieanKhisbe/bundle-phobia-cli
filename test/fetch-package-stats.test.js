75dc73ae8cd706f27bb17c92a268843cd366d221
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-09-12 13:42:44 +0200

    dev: implement a getVersionList to retrieve available list from npm

--- test/fetch-package-stats.test.js
+++ test/fetch-package-stats.test.js
@@ -1,7 +1,7 @@
 const fetch = require('jest-fetch-mock');
 jest.setMock('node-fetch', fetch);
 
-const fetchPackageStats = require('../lib/fetch-package-stats');
+const {fetchPackageStats, getVersionList} = require('../lib/fetch-package-stats');
 const {lodashStats, errorStats} = require('./fixtures');
 
 describe('fetchPackageStats', () => {
@@ -23,5 +23,22 @@ describe('fetchPackageStats', () => {
         return fetchPackageStats('yolodonotexist')
             .catch(err => expect(err.message).toEqual('The package you were looking for doesn\'t exist.'))
     });
+
+});
+
+describe('getVersionList', () => {
+
+    it('get the version list of an existing package', () => {
+        return getVersionList('lodash')
+            .then(versionList => {
+                expect(versionList.length > 50).toBeTruthy()
+                expect(versionList).toEqual(expect.arrayContaining(['0.1.0', '0.2.0', '4.12.0', '4.16.4']));
+            });
+    });
+
+    it('get the version list of an unknown package', () => {
+        return getVersionList('nonmaiscaexistepas')
+            .catch(err => expect(err.message).toEqual('Unknown Package nonmaiscaexistepas'));
+    })
 
 });
