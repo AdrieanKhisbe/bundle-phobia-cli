81d99d912912f6fd4fea7bc3ab0ecbbdddeb13fd
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-09-12 13:05:21 +0200

    feature: have spinner while loading information from the api

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
-const c = require('chalk');
+const c = require('chalk')
+const ora = require('ora');
 const fetchPackageStats = require('./lib/fetch-package-stats');
 const {syntheticView} = require('./lib/cli-views');
 
 if (!module.parent) {
-    fetchPackageStats(process.argv[2])
-        .then(packageState => console.log(syntheticView(packageState)))
+    const package = process.argv[2];
+    const spinner = ora(`Fetching stats for package ${c.dim.underline(package)}`).start()
+    fetchPackageStats(package)
+        .finally(() => spinner.stop().clear())
+        .then(packageState => {
+            console.log(syntheticView(packageState));
+        })
         .catch(err => console.error(c.red.bold('Error happened:'), err.message));
 }
