5a3f24197f01e26e793911ae05ea83cbbbd6bbb5
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-09-12 08:44:23 +0200

    init: draft of a synthetic view

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fetchPackageStats = require('./lib/fetch-package-stats');
+const {syntheticView} = require('./lib/cli-views');
 
 if (!module.parent) {
     fetchPackageStats(process.argv[2])
-        .then(packageState => console.log(packageState))
+        .then(packageState => console.log(syntheticView(packageState)))
         .catch(err => console.error('Error happened:', err.message));
 }
