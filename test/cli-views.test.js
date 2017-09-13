d4f3c05356a8388bbc92282dc58bfba17d5f211c
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-09-13 08:01:32 +0200

    test: cover the cli-views and the getView

--- test/cli-views.test.js
+++ test/cli-views.test.js
@@ -1,6 +1,6 @@
 const stripAnsi = require('strip-ansi');
 const {lodashStats} = require('./fixtures');
-const {syntheticView} = require('../lib/cli-views');
+const {getView, syntheticView, jsonView, sizeView, gzipsizeView, dependenciesView} = require('../lib/cli-views');
 
 describe('syntheticView', () => {
 
@@ -10,3 +10,47 @@ describe('syntheticView', () => {
     });
 
 });
+
+
+describe('simpleViews', () => {
+
+    it('sizeView', () => {
+        expect(sizeView(lodashStats)).toEqual(70870);
+    });
+    it('gzipsizeView', () => {
+        expect(gzipsizeView(lodashStats)).toEqual(24666);
+    });
+    it('dependenciesView', () => {
+        expect(dependenciesView(lodashStats)).toEqual(0);
+    });
+
+});
+
+
+
+describe('getView', () => {
+    it('retrieve synthetic view by default', () => {
+        expect(getView()).toEqual(syntheticView)
+    });
+
+    it('retrieve size view by demand', () => {
+        expect(getView({size: true})).toEqual(sizeView)
+    });
+
+    it('retrieve gzip size view by demand', () => {
+        expect(getView({'gzip-size': true})).toEqual(gzipsizeView)
+    });
+
+    it('retrieve dependencies view by demand', () => {
+        expect(getView({dependencies: true})).toEqual(dependenciesView)
+    });
+
+    it('retrieve dependencies view by demand', () => {
+        expect(getView({json: true})).toEqual(jsonView)
+    });
+
+    it('throw erreur when multiple args', () => {
+       expect(() => getView({json: true, size: true}).toThrow());
+    });
+
+})
