a9d17061c718835571ddb5d94639406a962a7902
Travis Fischer <fisch0920@gmail.com>
2018-06-11 10:32:44 -0400

    make size output easier to read via bytes

--- test/cli-views.test.js
+++ test/cli-views.test.js
@@ -6,7 +6,7 @@ describe('syntheticView', () => {
 
     it('basic lodash stats', () => {
         const lodashView = stripAnsi(syntheticView(lodashStats));
-        expect(lodashView).toEqual('lodash (4.17.4) has 0 dependencies for a weight of 70870B (24666B gzipped)');
+        expect(lodashView).toEqual('lodash (4.17.4) has 0 dependencies for a weight of 69.21KB (24.09KB gzipped)');
     });
 
 });
