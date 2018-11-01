65dd410e762a2697c9374261d38467b93f7a76a6
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-11-01 21:08:07 +0100

    dev: disable spinning when simple option is used

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const pkg = require('./package.json');
 
 const c = require('chalk')
 const Bromise = require('bluebird');
+const _ = require('lodash');
 const ora = require('ora');
 const {fetchPackageStats, getPackageVersionList} = require('./lib/fetch-package-stats');
 const {getView} = require('./lib/cli-views');
@@ -16,9 +17,11 @@ const argv = require('yargs')
     .describe('json', 'Output json rather than a formater string').alias('json', 'j').boolean('j')
     .describe('size', 'Output just the module size').alias('size', 's').boolean('s')
     .describe('gzip-size', 'Output just the module gzip size').alias('gzip-size', 'g').boolean('g')
-    .describe('dependencies', 'Output just thenumber of dependencies').alias('dependencies', 'd').boolean('d')
+    .describe('dependencies', 'Output just the number of dependencies').alias('dependencies', 'd').boolean('d')
     .help('h').alias('h', 'help')
     .argv;
+
+const singleOutput = argv => _.some(['size', 'json', 'gzip-size', 'dependencies'], opt => opt in argv);
 
 const main = argv => {;
     const fetchAndPresent = (computeMessage, fetchPromise, processFunction) => {
@@ -29,7 +32,7 @@ const main = argv => {;
             .catch(err => console.error(c.red.bold('Error happened:'), err.message));
     }
     const view = getView(argv);
-    const Spinner = fakeSpinner;
+    const Spinner = singleOutput(argv) ? fakeSpinner : ora;
     const spinner = Spinner(`Fetching stats for package${argv._.length > 1 ? 's list': ''}`).start()
     const packages = ('range' in argv && 'r' in argv)
       ? getPackageVersionList(argv._[0], argv.range ? argv.range : (argv.range === undefined ? 8 : 'all'))
@@ -44,7 +47,7 @@ const main = argv => {;
       .finally(() => spinner.stop())
 }
 
-module.exports.main = main;
+module.exports = {main, singleOutput};
 
 if (!module.parent) {
     main(argv)
