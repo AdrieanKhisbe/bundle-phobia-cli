19b9d6fb0571a4e81f6be319b14346fe22b80b1d
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-11-01 22:46:53 +0100

    refactor: prettier pass to reformat

--- test/cli-views.test.js
+++ test/cli-views.test.js
@@ -1,56 +1,57 @@
 const stripAnsi = require('strip-ansi');
 const {lodashStats} = require('./fixtures');
-const {getView, syntheticView, jsonView, sizeView, gzipsizeView, dependenciesView} = require('../lib/cli-views');
+const {
+  getView,
+  syntheticView,
+  jsonView,
+  sizeView,
+  gzipsizeView,
+  dependenciesView
+} = require('../lib/cli-views');
 
 describe('syntheticView', () => {
-
-    it('basic lodash stats', () => {
-        const lodashView = stripAnsi(syntheticView(lodashStats));
-        expect(lodashView).toEqual('lodash (4.17.4) has 0 dependencies for a weight of 69.21KB (24.09KB gzipped)');
-    });
-
+  it('basic lodash stats', () => {
+    const lodashView = stripAnsi(syntheticView(lodashStats));
+    expect(lodashView).toEqual(
+      'lodash (4.17.4) has 0 dependencies for a weight of 69.21KB (24.09KB gzipped)'
+    );
+  });
 });
-
 
 describe('simpleViews', () => {
-
-    it('sizeView', () => {
-        expect(sizeView(lodashStats)).toEqual(70870);
-    });
-    it('gzipsizeView', () => {
-        expect(gzipsizeView(lodashStats)).toEqual(24666);
-    });
-    it('dependenciesView', () => {
-        expect(dependenciesView(lodashStats)).toEqual(0);
-    });
-
+  it('sizeView', () => {
+    expect(sizeView(lodashStats)).toEqual(70870);
+  });
+  it('gzipsizeView', () => {
+    expect(gzipsizeView(lodashStats)).toEqual(24666);
+  });
+  it('dependenciesView', () => {
+    expect(dependenciesView(lodashStats)).toEqual(0);
+  });
 });
 
-
-
 describe('getView', () => {
-    it('retrieve synthetic view by default', () => {
-        expect(getView()).toEqual(syntheticView)
-    });
+  it('retrieve synthetic view by default', () => {
+    expect(getView()).toEqual(syntheticView);
+  });
 
-    it('retrieve size view by demand', () => {
-        expect(getView({size: true})).toEqual(sizeView)
-    });
+  it('retrieve size view by demand', () => {
+    expect(getView({size: true})).toEqual(sizeView);
+  });
 
-    it('retrieve gzip size view by demand', () => {
-        expect(getView({'gzip-size': true})).toEqual(gzipsizeView)
-    });
+  it('retrieve gzip size view by demand', () => {
+    expect(getView({'gzip-size': true})).toEqual(gzipsizeView);
+  });
 
-    it('retrieve dependencies view by demand', () => {
-        expect(getView({dependencies: true})).toEqual(dependenciesView)
-    });
+  it('retrieve dependencies view by demand', () => {
+    expect(getView({dependencies: true})).toEqual(dependenciesView);
+  });
 
-    it('retrieve dependencies view by demand', () => {
-        expect(getView({json: true})).toEqual(jsonView)
-    });
+  it('retrieve dependencies view by demand', () => {
+    expect(getView({json: true})).toEqual(jsonView);
+  });
 
-    it('throw erreur when multiple args', () => {
-       expect(() => getView({json: true, size: true}).toThrow());
-    });
-
-})
+  it('throw erreur when multiple args', () => {
+    expect(() => getView({json: true, size: true}).toThrow());
+  });
+});
--- test/fetch-package-stats.test.js
+++ test/fetch-package-stats.test.js
@@ -1,4 +1,5 @@
 const fetch = require('jest-fetch-mock');
+
 jest.setMock('node-fetch', fetch);
 
 const {fetchPackageStats, selectVersions} = require('../lib/fetch-package-stats');
@@ -8,44 +9,42 @@ jest.mock('../lib/npm-utils');
 const {getVersionList, resolveVersionRange} = require('../lib/npm-utils');
 
 describe('fetchPackageStats', () => {
-   resolveVersionRange.mockImplementation(name => Promise.resolve(name));
+  resolveVersionRange.mockImplementation(name => Promise.resolve(name));
 
-    it('simple get package', () => {
-        fetch.mockResponse(JSON.stringify(lodashStats));
-        return fetchPackageStats('lodash')
-            .then(stats => {
-            expect(stats).toEqual(lodashStats);
-        });
+  it('simple get package', () => {
+    fetch.mockResponse(JSON.stringify(lodashStats));
+    return fetchPackageStats('lodash').then(stats => {
+      expect(stats).toEqual(lodashStats);
     });
+  });
 
-    it('undefined package name', () => {
-        return fetchPackageStats()
-            .catch(err => expect(err.message).toEqual('Empty name given as argument'));
-    });
+  it('undefined package name', () => {
+    return fetchPackageStats().catch(err =>
+      expect(err.message).toEqual('Empty name given as argument')
+    );
+  });
 
-    it('unexisting package name', () => {
-        fetch.mockResponse(JSON.stringify(errorStats));
-        return fetchPackageStats('yolodonotexist')
-            .catch(err => expect(err.message).toEqual('The package you were looking for doesn\'t exist.'));
-    });
-
+  it('unexisting package name', () => {
+    fetch.mockResponse(JSON.stringify(errorStats));
+    return fetchPackageStats('yolodonotexist').catch(err =>
+      expect(err.message).toEqual("The package you were looking for doesn't exist.")
+    );
+  });
 });
 
 describe('selectVersions', () => {
+  it('select all the list implicitely', () => {
+    const list = ['1', '2', '3'];
+    expect(selectVersions(list, 4)).toEqual(['3', '2', '1']);
+  });
 
-    it('select all the list implicitely', () => {
-        const list = ['1', '2', '3']
-        expect(selectVersions(list, 4)).toEqual(['3', '2', '1'])
-    });
+  it('select all the list explicitely', () => {
+    const list = ['1', '2', '3'];
+    expect(selectVersions(list, 0)).toEqual(['3', '2', '1']);
+  });
 
-    it('select all the list explicitely', () => {
-        const list = ['1', '2', '3']
-        expect(selectVersions(list, 0)).toEqual(['3', '2', '1'])
-    });
-
-    it('select just the first elements', () => {
-        const list = ['1', '2', '3']
-        expect(selectVersions(list, 2)).toEqual(['3', '2'])
-    });
-
+  it('select just the first elements', () => {
+    const list = ['1', '2', '3'];
+    expect(selectVersions(list, 2)).toEqual(['3', '2']);
+  });
 });
