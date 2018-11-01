69238d286db7ca53406906605583674e4ce7d4e6
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-11-01 23:15:22 +0100

    refactor: relint and reformat the index file

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
 
 const updateNotifier = require('update-notifier');
-const pkg = require('./package.json');
 const c = require('chalk');
 const argv = require('yargs')
-    .usage('Usage: $0 <package-names...>')
-    .describe('range', 'Get a range of version (0 for all, 8 by default)').alias('range', 'r').number('range')
-    .describe('json', 'Output json rather than a formater string').alias('json', 'j').boolean('j')
-    .describe('size', 'Output just the module size').alias('size', 's').boolean('s')
-    .describe('gzip-size', 'Output just the module gzip size').alias('gzip-size', 'g').boolean('g')
-    .describe('dependencies', 'Output just the number of dependencies').alias('dependencies', 'd').boolean('d')
-    .help('h').alias('h', 'help')
-    .argv;
+  .usage('Usage: $0 <package-names...>')
+  .describe('range', 'Get a range of version (0 for all, 8 by default)')
+  .alias('range', 'r')
+  .number('range')
+  .describe('json', 'Output json rather than a formater string')
+  .alias('json', 'j')
+  .boolean('j')
+  .describe('size', 'Output just the module size')
+  .alias('size', 's')
+  .boolean('s')
+  .describe('gzip-size', 'Output just the module gzip size')
+  .alias('gzip-size', 'g')
+  .boolean('g')
+  .describe('dependencies', 'Output just the number of dependencies')
+  .alias('dependencies', 'd')
+  .boolean('d')
+  .help('h')
+  .alias('h', 'help').argv;
+const pkg = require('./package.json');
 
 const {main} = require('./src/core');
 
 if (!module.parent) {
-    main(argv).catch(err => {
-        console.log(c.red(err.message))
-        process.exit(1);
-    })
-    updateNotifier({pkg}).notify();
+  /* eslint-disable no-console */
+  main(argv).catch(err => {
+    console.log(c.red(err.message));
+    process.exit(1);
+  });
+  updateNotifier({pkg}).notify();
 }
