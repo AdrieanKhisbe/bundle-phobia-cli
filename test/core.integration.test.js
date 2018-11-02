b4097a4c1787f71605e5f09c477c3eb21ee6dd58
Adriean Khisbe <adriean.khisbe@live.fr>
2018-11-02 09:51:20 +0100

    test: add some more integration tests

--- test/core.integration.test.js
+++ test/core.integration.test.js
@@ -17,7 +17,7 @@ describe('Integrations tests', () => {
   it('fetch just a single package', done => {
     const stream = fakeStream();
     // had to pin version for test stability
-    return main({argv: {_: ['lodash@4.12.0']}, stream, noOra: true})
+    return main({argv: {_: ['lodash@4.12.0']}, stream})
       .then(() => {
         const output = stream.getContent();
         expect(stripAnsi(output)).toEqual(
@@ -29,5 +29,57 @@ describe('Integrations tests', () => {
         return done();
       })
       .catch(done);
+  });
+  it('fetch just a list of package', done => {
+    const stream = fakeStream();
+    // had to pin version for test stability
+    return main({argv: {_: ['react@15', 'lodash@2', 'moment@1.2']}, stream})
+      .then(() => {
+        const output = stream.getContent();
+        expect(stripAnsi(output)).toEqual(
+          `- Fetching stats for package react@15
+ℹ react (15.0.0) has 3 dependencies for a weight of 19.96KB (6.46KB gzipped)
+- Fetching stats for package lodash@2
+ℹ lodash (2.0.0) has 0 dependencies for a weight of 26.17KB (9.52KB gzipped)
+- Fetching stats for package moment@1.2
+ℹ moment (1.2.0) has 0 dependencies for a weight of 113.98KB (14.77KB gzipped)
+`
+        );
+
+        return done();
+      })
+      .catch(done);
+  });
+  it('fetch just a list of package', done => {
+    const stream = fakeStream();
+    // had to pin version for test stability
+    return main({
+      argv: {_: ['react@15', 'lodash@2', 'moment@1.2'], range: 2, r: 2},
+      stream
+    })
+      .catch(err => {
+        expect(err.message).toEqual("Can't use both --range option and list of packages");
+      })
+      .asCallback(done);
+  });
+  it('fetch a package that does not exist at all', done => {
+    const stream = fakeStream();
+    // had to pin version for test stability
+    return main({
+      argv: {_: ['sorry-but-i-really-do-not-exist']},
+      stream
+    })
+      .then(() => {
+        const output = stream.getContent();
+        return expect(stripAnsi(output)).toEqual(
+          `- Fetching stats for package sorry-but-i-really-do-not-exist
+✖ resolving sorry-but-i-really-do-not-exist failed: The package you were looking for doesn't exist.
+`
+        );
+      })
+      .catch(err => {
+        expect(err.message).toEqual("Can't use both --range option and list of packages");
+      })
+      .asCallback(done);
   });
 });
