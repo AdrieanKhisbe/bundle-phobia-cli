de97bd8b646353b647c002f982102bf8f76237e7
Adriean Khisbe <adriean.khisbe@live.fr>
2018-11-02 10:17:14 +0100

    dev: customize the failling error output when error not catch

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const {main} = require('./src/core');
 if (!module.parent) {
   /* eslint-disable no-console */
   main({argv}).catch(err => {
-    console.log(c.red(err.message));
+    console.log(c.red.bold('bundle-phobia failed: ') + err.message);
     process.exit(1);
   });
   updateNotifier({pkg}).notify();
