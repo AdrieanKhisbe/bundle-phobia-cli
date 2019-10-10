3e455b13707a32360910802973f5784c63025637
Adriean Khisbe <adriean.khisbe@live.fr>
2019-10-10 09:25:22 +0200

    test: refactor tests to use async/await #betterReadability

--- test/core.integration.test.js
+++ test/core.integration.test.js
@@ -15,85 +15,74 @@ const fakeStream = () => {
 };
 
 describe('Integrations tests', () => {
-  it('fetch just a single package', done => {
+  it('fetch just a single package', async () => {
     const stream = fakeStream();
     // had to pin version for test stability
-    return main({argv: {_: ['lodash@4.12.0']}, stream})
-      .then(() => {
-        const output = stream.getContent();
-        expect(stripAnsi(output)).toEqual(
-          `- Fetching stats for package lodash@4.12.0
+    await main({argv: {_: ['lodash@4.12.0']}, stream});
+
+    const output = stream.getContent();
+    expect(stripAnsi(output)).toEqual(
+      `- Fetching stats for package lodash@4.12.0
 ℹ lodash (4.12.0) has 0 dependencies for a weight of 63.65KB (22.11KB gzipped)
 `
-        );
-
-        return done();
-      })
-      .catch(done);
+    );
   });
-  it('fetch just a list of package', done => {
+  it('fetch just a list of package', async () => {
     const stream = fakeStream();
     // had to pin version for test stability
-    return main({argv: {_: ['lodash@2.4.2', 'moment@1.2.0']}, stream})
-      .then(() => {
-        const output = stream.getContent();
-        expect(stripAnsi(output)).toEqual(
-          `- Fetching stats for package lodash@2.4.2
+    await main({argv: {_: ['lodash@2.4.2', 'moment@1.2.0']}, stream});
+
+    const output = stream.getContent();
+    expect(stripAnsi(output)).toEqual(
+      `- Fetching stats for package lodash@2.4.2
 ℹ lodash (2.4.2) has 0 dependencies for a weight of 27.94KB (10.04KB gzipped)
 - Fetching stats for package moment@1.2.0
 ℹ moment (1.2.0) has 0 dependencies for a weight of 114.1KB (14.83KB gzipped)
 
 ℹ total (2 packages) has 0 dependencies for a weight of 142.04KB (24.87KB gzipped)
 `
-        );
-
-        return done();
-      })
-      .catch(done);
+    );
   }, 10000);
-  it('fetch just a list of package', done => {
+  it('fetch just a list of package', async () => {
     const stream = fakeStream();
-    // had to pin version for test stability
-    return main({
-      argv: {_: ['react@15', 'lodash@2', 'moment@1.2'], range: 2, r: 2},
-      stream
-    })
-      .catch(err => {
-        expect(err.message).toEqual("Can't use both --range option and list of packages");
-      })
-      .asCallback(done);
+    try {
+      await main({
+        argv: {_: ['react@15', 'lodash@2', 'moment@1.2'], range: 2, r: 2},
+        stream
+      });
+      throw new Error('Did not fail as expected');
+    } catch (err) {
+      expect(err.message).toEqual("Can't use both --range option and list of packages");
+    }
   });
 
-  it('fetch a package that does not exist at all', done => {
+  it('fetch a package that does not exist at all', async () => {
     const stream = fakeStream();
-    // had to pin version for test stability
-    return main({
+    await main({
       argv: {_: ['sorry-but-i-really-do-not-exist']},
       stream
-    })
-      .then(() => {
-        const output = stream.getContent();
-        return expect(stripAnsi(output)).toEqual(
-          `- Fetching stats for package sorry-but-i-really-do-not-exist
+    });
+    const output = stream.getContent();
+    expect(stripAnsi(output)).toEqual(
+      `- Fetching stats for package sorry-but-i-really-do-not-exist
 ✖ resolving sorry-but-i-really-do-not-exist failed: The package you were looking for doesn't exist.
 `
-        );
-      })
-      .asCallback(done);
+    );
   });
 
-  it('fetch a package that does not exist at all with range', done => {
+  it('fetch a package that does not exist at all with range', async () => {
     const stream = fakeStream();
-    // had to pin version for test stability
-    return main({
-      argv: {_: ['sorry-but-i-really-do-not-exist'], range: 2, r: 2},
-      stream
-    })
-      .catch(err => {
-        expect(err.message).toEqual(
-          "sorry-but-i-really-do-not-exist: The package you were looking for doesn't exist."
-        );
-      })
-      .asCallback(done);
+    try {
+      await main({
+        argv: {_: ['sorry-but-i-really-do-not-exist'], range: 2, r: 2},
+        stream
+      });
+
+      throw new Error('Did not fail as expected');
+    } catch (err) {
+      expect(err.message).toEqual(
+        "sorry-but-i-really-do-not-exist: The package you were looking for doesn't exist."
+      );
+    }
   });
 });
--- test/install.integration.test.js
+++ test/install.integration.test.js
@@ -36,165 +36,148 @@ const fakePrompt = (result = true) => {
 
 const defaultMaxSize = 10000;
 describe('Integrations tests', () => {
-  it('install just a single package and fail', done => {
+  it('install just a single package and fail', async () => {
     const stream = fakeStream();
     const exec = fakeExec();
-    // had to pin version for test stability
-    return main({
-      argv: {_: ['lodash@4.12.0']},
-      stream,
-      exec,
-      defaultMaxSize,
-      readPkg: fakePkg
-    })
-      .catch(err => {
-        expect(err.message).toEqual('Install was canceled.');
-        const output = stream.getContent();
-        expect(stripAnsi(output)).toEqual(
-          `ℹ Applying a size limit of 9.77KB from default
+    //
+    try {
+      await main({
+        argv: {_: ['lodash@4.12.0']},
+        stream,
+        exec,
+        defaultMaxSize,
+        readPkg: fakePkg
+      });
+      throw new Error('Did not fail as exected');
+    } catch (err) {
+      expect(err.message).toEqual('Install was canceled.');
+      const output = stream.getContent();
+      expect(stripAnsi(output)).toEqual(
+        `ℹ Applying a size limit of 9.77KB from default
 
 - Fetching stats for package lodash@4.12.0
 ✖ Could not install for following reasons:
 ✖ lodash@4.12.0: size over threshold (63.65KB > 9.77KB)
 ✔ global constraint is respected
 `
-        );
-
-        return done();
-      })
-      .catch(done);
+      );
+    }
   });
-  it('install just a single package and succeed', done => {
+  it('install just a single package and succeed', async () => {
     const stream = fakeStream();
     const exec = fakeExec();
-    // had to pin version for test stability
-    return main({
+    //
+    await main({
       argv: {_: ['bytes@3.0.0']},
       stream,
       exec,
       defaultMaxSize,
       readPkg: fakePkg
-    })
-      .then(() => {
-        const output = stream.getContent();
-        expect(stripAnsi(output)).toEqual(
-          `ℹ Applying a size limit of 9.77KB from default
+    });
+
+    const output = stream.getContent();
+    expect(stripAnsi(output)).toEqual(
+      `ℹ Applying a size limit of 9.77KB from default
 
 - Fetching stats for package bytes@3.0.0
 ℹ Proceed to installation of package bytes@3.0.0
 `
-        );
-        expect(exec.retrieveCmd()).toEqual('npm install bytes@3.0.0');
-
-        return done();
-      })
-      .catch(done);
+    );
+    expect(exec.retrieveCmd()).toEqual('npm install bytes@3.0.0');
   });
-  it('install just a single package and just warn', done => {
+  it('install just a single package and just warn', async () => {
     const stream = fakeStream();
     const exec = fakeExec();
-    // had to pin version for test stability
-    return main({
+    //
+    await main({
       argv: {_: ['lodash@4.12.0'], w: true, warn: true, 'save-dev': true},
       stream,
       exec,
       defaultMaxSize,
       readPkg: fakePkg
-    })
-      .then(() => {
-        const output = stream.getContent();
-        expect(stripAnsi(output)).toEqual(
-          `ℹ Applying a size limit of 9.77KB from default
+    });
+
+    const output = stream.getContent();
+    expect(stripAnsi(output)).toEqual(
+      `ℹ Applying a size limit of 9.77KB from default
 
 - Fetching stats for package lodash@4.12.0
 ⚠ Proceed to installation of packages lodash@4.12.0 despite following warnings:
 ⚠ lodash@4.12.0: size over threshold (63.65KB > 9.77KB)
 `
-        );
-        expect(exec.retrieveCmd()).toEqual('npm install lodash@4.12.0 --save-dev');
-        return done();
-      })
-      .catch(done);
+    );
+    expect(exec.retrieveCmd()).toEqual('npm install lodash@4.12.0 --save-dev');
   });
 
-  it('ask to install a package and accept', done => {
+  it('ask to install a package and accept', async () => {
     const stream = fakeStream();
     const exec = fakeExec();
     const prompt = fakePrompt();
-    // had to pin version for test stability
-    return main({
+    //
+    await main({
       argv: {_: ['lodash@4.12.0'], i: true, interactive: true},
       stream,
       exec,
       prompt,
       defaultMaxSize,
       readPkg: fakePkg
-    })
-      .then(() => {
-        const output = stream.getContent();
-        expect(stripAnsi(output)).toEqual(
-          `ℹ Applying a size limit of 9.77KB from default
+    });
+    const output = stream.getContent();
+    expect(stripAnsi(output)).toEqual(
+      `ℹ Applying a size limit of 9.77KB from default
 
 - Fetching stats for package lodash@4.12.0
 ⚠ Packages lodash@4.12.0 raised following warnings:
 ⚠ lodash@4.12.0: size over threshold (63.65KB > 9.77KB)
 ✔ Proceeding with installation as you requested
 `
-        );
-        expect(exec.retrieveCmd()).toEqual('npm install lodash@4.12.0');
-        return done();
-      })
-      .catch(done);
+    );
+    expect(exec.retrieveCmd()).toEqual('npm install lodash@4.12.0');
   });
-  it('ask to install a package and deny', done => {
+  it('ask to install a package and deny', async () => {
     const stream = fakeStream();
     const exec = fakeExec();
     const prompt = fakePrompt(false);
-    // had to pin version for test stability
-    return main({
+    //
+    await main({
       argv: {_: ['lodash@4.12.0'], i: true, interactive: true},
       stream,
       exec,
       prompt,
       defaultMaxSize,
       readPkg: fakePkg
-    })
-      .then(() => {
-        const output = stream.getContent();
-        expect(stripAnsi(output)).toEqual(
-          `ℹ Applying a size limit of 9.77KB from default
+    });
+
+    const output = stream.getContent();
+    expect(stripAnsi(output)).toEqual(
+      `ℹ Applying a size limit of 9.77KB from default
 
 - Fetching stats for package lodash@4.12.0
 ⚠ Packages lodash@4.12.0 raised following warnings:
 ⚠ lodash@4.12.0: size over threshold (63.65KB > 9.77KB)
 ✖ Installation is canceled on your demand
 `
-        );
-        expect(exec.retrieveCmd()).toBeUndefined();
-        return done();
-      })
-      .catch(done);
+    );
+    expect(exec.retrieveCmd()).toBeUndefined();
   });
 
-  it('try to install package that does not exist', done => {
+  it('try to install package that does not exist', async () => {
     const stream = fakeStream();
     const exec = fakeExec();
-    // had to pin version for test stability
-    return main({
-      argv: {_: ['no-sorry-but-i-do-not-exist']},
-      stream,
-      exec,
-      defaultMaxSize,
-      readPkg: fakePkg
-    })
-      .then(() => {
-        throw new Error('Exception was not triggered');
-      })
-      .catch(err => {
-        expect(err.message).toEqual(
-          "no-sorry-but-i-do-not-exist: The package you were looking for doesn't exist."
-        );
-        return done();
+    try {
+      await main({
+        argv: {_: ['no-sorry-but-i-do-not-exist']},
+        stream,
+        exec,
+        defaultMaxSize,
+        readPkg: fakePkg
       });
+
+      throw new Error('Exception was not triggered');
+    } catch (err) {
+      expect(err.message).toEqual(
+        "no-sorry-but-i-do-not-exist: The package you were looking for doesn't exist."
+      );
+    }
   });
 });
