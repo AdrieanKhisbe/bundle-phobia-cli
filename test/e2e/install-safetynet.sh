30099f287b5188ec81367e16e5b8d09823575b35
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-12-07 22:31:13 +0100

    Avoid using bash <(magic) in install safetynet :mage:

--- test/e2e/install-safetynet.sh
+++ test/e2e/install-safetynet.sh
@@ -3,8 +3,7 @@ set -e
 
 ts=$(date +"%s")
 
-E2E_FOLDER="${BASH_SOURCE%/*}"
-
+E2E_FOLDER="$(dirname "$(realpath "$0")")"
 mkdir -p $E2E_FOLDER/tmp
 sandbox="$E2E_FOLDER/tmp/package-$ts"
 output_file="$E2E_FOLDER/tmp/output-$ts"
@@ -12,7 +11,7 @@ cp -r $E2E_FOLDER/package $sandbox
 
 
 args="lodash@4.12 --warn"
-expected_output="$(cat<<EXPECTED_OUTPUT
+cat > $E2E_FOLDER/tmp/expected_output <<EXPECTED_OUTPUT
 ℹ Applying a size limit of 20KB from argv and overall size limit of 50KB from argv
 
 - Fetching stats for package lodash@4.12
@@ -20,12 +19,11 @@ expected_output="$(cat<<EXPECTED_OUTPUT
 ⚠ lodash@4.12: size over threshold (63.65KB > 20KB)
 ⚠ global constraint is not respected: overall size after install would be over threshold (0B installed + 63.65KB > 50KB)
 EXPECTED_OUTPUT
-)"
 
-(cd $sandbox && node ../../../../index-install $args) > $output_file 2> $output_file.err
+(cd $sandbox && node ../../../../index-install $args > $output_file 2> $output_file.err)
 
 echo "Program was successful, checking output"
-if ! diff <(tail -n +2 $output_file) <(echo "$expected_output"); then
+if ! diff $output_file $E2E_FOLDER/tmp/expected_output; then
     echo output was not exactly the one expected, see output:
     cat $output_file
     echo "See also stderr:"
