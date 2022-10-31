8b4a592ed5795b585b1f5ebc3ec1f3b478b3b4a4
AdrieanKhisbe <adriean.khisbe@live.fr>
2022-10-31 12:19:54 +0100

    Adapt tests to support different npm messages :wrench:

--- test/e2e/install-safetynet.sh
+++ test/e2e/install-safetynet.sh
@@ -18,24 +18,48 @@ cat > $E2E_FOLDER/tmp/expected_output <<EXPECTED_OUTPUT
 ⚠ Proceed to installation of packages lodash@4.12 despite following warnings:
 ⚠ lodash@4.12: size over threshold (63.65KB > 20KB)
 ⚠ global constraint is not respected: overall size after install would be over threshold (0B installed + 63.65KB > 50KB)
-+ lodash@4.12.0
 EXPECTED_OUTPUT
 
+set +e
 (cd $sandbox && node ../../../../index-install $args > $output_file 2> $output_file.err)
+status_code=$?
+set -e
+head -6 $output_file > $output_file.head
 
-head -7 $output_file > $output_file.head
+if [ $status_code -ne 0 ]; then
+    echo "Install failed, returned with $status_code exit code"
+    exit $status_code
+fi
+
+
 echo "Program was successful, checking output"
 if ! diff $output_file.head $E2E_FOLDER/tmp/expected_output; then
     echo output was not exactly the one expected, see output:
     cat $output_file
     echo "See also stderr:"
     cat $output_file.err
-    exit 2
+    exit_status=2
 fi
 if ! grep lodash $sandbox/package.json > /dev/null; then
     echo lodash was not installed it seems
-    exit 3
+    exit_status=3
 fi
-echo "Safety net got the expected results"
+
+node_major=$(node <<< "console.log(process.versions.node.split('.')[0])")
+case $node_major in
+    8|10|12|14) expected_add_message="added 1 package from 3 contributors and audited 2 packages in";;
+    16|18) expected_add_message="added 1 package, and audited 3 packages";;
+    *) expected_add_message="><"; echo "No expected message for this major version";;
+esac
+
+# output from npm diverges between node 14 and 16
+if ! cat $output_file | grep -q "$expected_add_message"; then
+    exit_status=1
+fi
 
 rm -r $sandbox
+if [ ${exit_status:-0} -eq 0 ]; then
+    echo "Safety net got the expected results"
+else
+    exit $exit_status
+fi
